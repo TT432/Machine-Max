4bd8c61441aef98b06774aee80a4627b3ebbb03d
TT432 <2437315224@qq.com>
2024-09-26 22:01:07 +0800

    :tada:

---
+++ src/main/java/io/github/tt432/machinemax/MachineMax.java
@@ -0,0 +1,8 @@
+package io.github.tt432.machinemax;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(MachineMax.MOD_ID)
+public class MachineMax {
+    public static final String MOD_ID = "machine_max";
+}
