b51068bc44f2d43c74ff2f24c59b2f5d00421461
Sweetzonzi <1368003448@qq.com>
2024-11-19 03:11:10 +0800

    前进后退操纵测试

--- src/main/java/io/github/tt432/machinemax/common/entity/controller/CarController.java
+++ src/main/java/io/github/tt432/machinemax/common/entity/controller/CarController.java
@@ -2,6 +2,7 @@ package io.github.tt432.machinemax.common.entity.controller;
 
 import io.github.tt432.machinemax.common.entity.entity.BasicEntity;
 import io.github.tt432.machinemax.common.part.slot.AbstractPartSlot;
+import io.github.tt432.machinemax.utils.physics.ode.DHinge2Joint;
 
 public class CarController extends PhysController {
 
@@ -12,14 +13,14 @@ public class CarController extends PhysController {
     @Override
     public void applyAllEffects() {
         AbstractPartSlot slot;
-//        slot =controlledEntity.CORE_PART.children_parts.get(0);
-//        ((DHinge2Joint)slot.joints.getFirst()).addTorques(0,150);
-//        slot =controlledEntity.CORE_PART.children_parts.get(1);
-//        ((DHingeJoint)slot.joints.getFirst()).addTorque(150);
-//        slot =controlledEntity.CORE_PART.children_parts.get(2);
-//        ((DHingeJoint)slot.joints.getFirst()).addTorque(150);
-//        slot =controlledEntity.CORE_PART.children_parts.get(3);
-//        ((DHinge2Joint)slot.joints.getFirst()).addTorques(0,150);
+        slot =controlledEntity.corePart.children_parts.get(0);
+        ((DHinge2Joint)slot.joints.getFirst()).addTorques(0,rawMoveInput[2]);
+        slot =controlledEntity.corePart.children_parts.get(1);
+        ((DHinge2Joint)slot.joints.getFirst()).addTorques(0,rawMoveInput[2]);
+        slot =controlledEntity.corePart.children_parts.get(2);
+        ((DHinge2Joint)slot.joints.getFirst()).addTorques(0,rawMoveInput[2]);
+        slot =controlledEntity.corePart.children_parts.get(3);
+        ((DHinge2Joint)slot.joints.getFirst()).addTorques(0,rawMoveInput[2]);
         super.applyAllEffects();
     }
 }
