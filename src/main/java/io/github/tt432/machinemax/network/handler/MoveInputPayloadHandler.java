c2ada4bb137e971a80751ad5349eda3a639e1274
Sweetzonzi <1368003448@qq.com>
2024-11-27 02:02:30 +0800

    部分修复轮胎转向鬼畜问题

--- src/main/java/io/github/tt432/machinemax/network/handler/MoveInputPayloadHandler.java
+++ src/main/java/io/github/tt432/machinemax/network/handler/MoveInputPayloadHandler.java
@@ -1,19 +1,15 @@
 package io.github.tt432.machinemax.network.handler;
 
-import io.github.tt432.machinemax.MachineMax;
 import io.github.tt432.machinemax.common.entity.entity.BasicEntity;
-import io.github.tt432.machinemax.network.payload.MoveInputPayload;
-import net.minecraft.client.multiplayer.ServerData;
+import io.github.tt432.machinemax.network.payload.MovementInputPayload;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.player.Player;
 import net.neoforged.neoforge.network.PacketDistributor;
 import net.neoforged.neoforge.network.handling.IPayloadContext;
 
-import java.util.Arrays;
-
 public class MoveInputPayloadHandler {
 
-    public static void clientHandler(final MoveInputPayload payload, final IPayloadContext context) {
+    public static void clientHandler(final MovementInputPayload payload, final IPayloadContext context) {
         //将其他玩家的输入同步至本机，以在客户端模拟其他玩家的操作
         //TODO:测试操作延迟情况
         Player player = context.player();
@@ -23,7 +19,7 @@ public class MoveInputPayloadHandler {
         }
     }
 
-    public static void serverHandler(final MoveInputPayload payload, final IPayloadContext context) {
+    public static void serverHandler(final MovementInputPayload payload, final IPayloadContext context) {
         Player player = context.player();
         if (player.getVehicle() instanceof BasicEntity e && e.getController() != null) {
             e.getController().setRawMoveInput(payload.input());
